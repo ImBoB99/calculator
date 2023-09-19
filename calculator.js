e57550b901c638e202ecad3389094c695ac93c47
Dominik Stefancic <119432963+ImBoB99@users.noreply.github.com>
2023-09-19 12:08:35 +0200

    Finish basic calculator

--- calculator.js
+++ calculator.js
@@ -1,21 +1,44 @@
 let firstNumber;
 let secondNumber;
 let operator;
+let result;
+let hasOperator = false;
 
 function add (a, b) {
-    return a + b;
+    result =  a + b;
+    hasOperator = false;
+    displayResult(result);
 }
 
 function subtract (a, b) {
-    return a - b;
+    result =  a - b;
+    hasOperator = false;
+    displayResult(result);
 }
 
 function multiply (a, b) {
-    return a * b;
+    result =  a * b;
+    hasOperator = false;
+    displayResult(result);
 }
 
 function divide (a, b) {
-    return a / b;
+    if (a === 0 || b === 0) {
+        result = null;
+    } else {
+        result =  a / b;
+    }
+    hasOperator = false;
+    displayResult(result);
+}
+
+function displayResult (result) {
+    if (result === null) {
+        return calculatorDisplay.innerText = "";
+    } else {
+        return calculatorDisplay.innerText = parseFloat(result.toFixed(5));
+    }
+    
 }
 
 function operate (firstNumber, secondNumber, operator) {
@@ -28,15 +51,41 @@ function operate (firstNumber, secondNumber, operator) {
     } else if (operator === "/") {
         return divide(firstNumber, secondNumber);
     }
+
 }
 
 function populate () {
     const value = this.getAttribute("data-value");
-    console.log(value);
+
+    
+    if (value === "=") {
+        const calculationString = calculatorDisplay.textContent;
+        const match = calculationString.match(/(\d+\.?\d*)\s*([+\-*/])\s*(\d+\.?\d*)/);
+        
+        if (match) {
+            firstNumber = parseFloat(match[1]);
+            operator = match[2];
+            secondNumber = parseFloat(match[3]);
+            operate(firstNumber, secondNumber, operator);
+        }
+
+    } else if (value === "del") {
+        displayResult(null);
+        hasOperator = false;
+
+    } else {
+        if (value.match(/[+*\-/]/) && hasOperator == false) {
+            hasOperator = true;
+            calculatorDisplay.innerText += value;
+        } else if (value.match(/[0-9]/)) {
+            calculatorDisplay.innerText += value;
+        }
+
+    }
+    
 }
 
 const calculatorDisplay = document.querySelector(".calculator-display p");
-calculatorDisplay.innerText = "69696969";
 
 const buttons = document.querySelectorAll("button");
 
